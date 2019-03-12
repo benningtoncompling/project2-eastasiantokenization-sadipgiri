d04498e486623c133f2ec0af76690ca84cdc79db
Sadip Giri <sadipgiri@bennington.edu>
2019-03-12 13:44:12 -0400

    complete maxmatch algo and japanese tokenizer project 2

---
+++ Japanese Tokenizer/accuracy.py
@@ -0,0 +1,12 @@
+'''
+    accuracy.py - Python3 program to return accuracy of Word Segmentation comparing tow files:
+        i.e. (1)gold standard file with (2)segmented files
+        NOTE: it matches the each line of those two files and returns the accuracy percentage of our Segmentation
+    Auhtor: Sadip Giri (sadipgiri@bennington.edu)
+    Date: 12the March, 2019
+'''
+
+
+if __name__ == '__main__':
+    main()
+    
